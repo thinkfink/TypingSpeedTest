3c98fd7b6e4d8373e89f945addc163b31cd018f0
Tyler <tylerfink95@gmail.com>
2020-12-27 18:29:10 -0600

    array of words displayed randomly

--- script.js
+++ script.js
@@ -1,27 +1,43 @@
 $(function(){
-    var arrWords = new Array("by","see","change","or","miss","you","though","away","those","got","much","mountain","line","went","below","these","night","way","begin","feet","as","most","might","every","life","into","such","write","people","off","hard","many","if","each","sometimes","go","eat","know","carry","just",);
+    var words = [
+        "by","see","change","or","miss","you","though","away","those","got","much","mountain","line","went","below","these","night","way","begin","feet","as","most","might","every","life","into","such","write","people","off","hard","many","if","each","sometimes","go","eat","know","carry","just"
+    ];
+    var Arr_words = new Array();
     var wordsDisplayElement = document.getElementById('wordsDisplay');
     var wordsInputElement = document.getElementById('wordsInput');
     var timerElement = document.getElementById('timer');
     var seconds = 60;
+    var wordCount = 40;
+
+    function displayWords(){
+        for(var i = 0; i < wordCount; i++){
+			var randomWord = Math.floor(Math.random() * words.length);
+			Arr_words.push(words[randomWord]);
+			//words.splice(randomWord, 1);
+        }
+        $(wordsDisplayElement).append(Arr_words);
+    }
 
     function countdown(minutes){
         var mins = minutes;
         function tick() {
-            var timer = document.getElementById("timer");
+            var timer = timerElement;
             var currentMinutes = mins-1
             seconds--;
             timer.innerHTML = "<p class='display-3'>" + currentMinutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds) + "</p>";
-            if( seconds > 0 ) {
+            if(seconds > 0) {
                 setTimeout(tick, 1000);
             } else {
                 if(mins > 1){
-                    countdown(mins-1);           
+                    countdown(mins-1);
                 }
             }
         }
         tick();
      }
+
+    //$(wordsDisplayElement).append(arrWords);
+    displayWords();
 
     $(wordsInputElement).keydown(function startCountdown(){
         if(seconds == 60){
@@ -37,6 +53,4 @@ $(function(){
         location.reload();
         return false;
     });
-
-
 });
